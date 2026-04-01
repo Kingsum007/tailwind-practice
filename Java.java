3b0c6c113abe7a742c4f05961943d6945828990e
Safi Ullah Mirzai <sumirzai@gmail.com>
2026-04-01 21:31:26 +0430

    Reuse Styles

---
+++ Java.java
@@ -0,0 +1,8 @@
+class Java{
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c= a+b;
+        System.out.println("The Addition of "+ a +" and "+ b +" is: "+c);
+    }
+}
---
+++ main.dart
@@ -0,0 +1,6 @@
+void main() {
+  var a = 1;
+  var b = 2;
+  var c = a + b;
+  print("The addition is $c");
+}
