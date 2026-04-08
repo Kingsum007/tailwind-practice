1b218f9cd2caa61e09ec42d1265e3801f33ec3d9
Safi Ullah Mirzai <sumirzai@gmail.com>
2026-04-08 21:39:50 +0430

    Columns and Container

--- Java.java
+++ Java.java
@@ -1,8 +1,31 @@
 class Java{
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        int c= a+b;
-        System.out.println("The Addition of "+ a +" and "+ b +" is: "+c);
+        // int a = 1;
+        // int b = 2;
+        // int c= a+b;
+        // System.out.println("The Addition of "+ a +" and "+ b +" is: "+c);
+        int n = 20;
+        String r = (n%2==0)?"Even":"Odd";
+        System.out.println(r);
+
+        int r1 = n%2;
+
+        if(r1==0)
+        {
+            System.out.println("The Number is Even");
+        }
+        else
+        {
+            System.out.println("The Number is Odd");
+        }
+
+
+        int x = 32;
+        int y = x%3;
+
+        System.out.println(y);
+
+        
+
     }
 }
