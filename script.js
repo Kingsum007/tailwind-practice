57654c2c72a3ebad36d5ed8346d3603668655464
Safi Ullah Mirzai <sumirzai@gmail.com>
2026-04-14 20:26:26 +0430

    Display

---
+++ script.js
@@ -0,0 +1,23 @@
+function setDisplay(type) {
+  const container = document.getElementById("container");
+
+  container.className = "border p-4 min-h-[150px]";
+  if (type === "block") {
+    container.classList.add("block");
+  }
+  if (type === "inline") {
+    container.classList.add("inline");
+  }
+  if (type === "inline-block") {
+    container.classList.add("inline-block");
+  }
+  if (type === "flex") {
+    container.classList.add("flex", "gap-2");
+  }
+  if (type === "grid") {
+    container.classList.add("grid", "gap-2");
+  }
+  if (type === "hidden") {
+    container.classList.add("hidden");
+  }
+}
